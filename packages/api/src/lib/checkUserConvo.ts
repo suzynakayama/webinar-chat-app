1770d1c62dfbb0987127851f86e628613989ed37
Suzy <suzy.nakayama@gmail.com>
2020-04-12 23:32:08 -0400

    added try/catch to check user convo

--- packages/api/src/lib/checkUserConvo.ts
+++ packages/api/src/lib/checkUserConvo.ts
@@ -1,12 +1,17 @@
 import { UserConversation } from "../models/UserConversation";
 
 export const checkUserConvo = async (userID: string, conversationID: string) => {
-    const exists = await UserConversation.findOne({
-    where: {
-      userID,
-      conversationID
-    }
-  });
-  if (!exists) throw new Error('You do not have permission to that conversation');
+  let exists;
+  try {
+    exists = await UserConversation.findOne({
+      where: {
+        userID,
+        conversationID
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    if (!exists) throw new Error('You do not have permission to that conversation');
+  }
   return true;
-}
+};
